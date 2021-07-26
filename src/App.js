8402434a5c369a7f12e44ae9dc658b9aae4203f7
Teeegg <shaun.n10b@gmail.com>
2021-07-26 15:54:05 +1000

    pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites'
+
 function App() {
-  return <div>Hello!</div>;
+  return <div>
+    <Route path='/'>
+      <AllMeetupsPage />
+    </Route>
+    <Route path='/new-meetup'>
+      <NewMeetupPage />
+    </Route>
+    <Route path='/favorites'>
+      <FavoritesPage />
+    </Route>
+  </div>;
 }
 
 export default App;
